cec9d22b823712bed19176b9485c4f13717bcd80
DouyGatien <gatien.douy@oclock.school>
2024-02-09 17:46:05 +0100

    SportController added

---
+++ src/Form/SportType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Sport;
+use App\Entity\Spot;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SportType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('spot_id', EntityType::class, [
+                'class' => Spot::class,
+'choice_label' => 'id',
+'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sport::class,
+        ]);
+    }
+}
