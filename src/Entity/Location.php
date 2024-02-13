0486d2f09f2fa436598bddc23d12c83dd9bf0e53
OmbelinePinoche <ombeline.pinoche@oclock.school>
2024-02-13 11:17:15 +0100

    location http request

--- src/Entity/Location.php
+++ src/Entity/Location.php
@@ -11,15 +11,17 @@ use Symfony\Component\Serializer\Annotation\Groups;
 #[ORM\Entity(repositoryClass: LocationRepository::class)]
 class Location
 {
+    #[Groups(['api_list_location', 'api_show_location'])]
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[Groups(['list', 'show', 'api_list', 'api_show', 'api_show_by_sport', 'show_by_sport'])]
+    #[Groups(['api_list_location', 'api_show_location', 'list', 'show', 'api_list', 'api_show', 'api_show_by_sport', 'show_by_sport'])]
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
+    #[Groups(['api_list_location', 'api_show_location'])]
     #[ORM\OneToMany(targetEntity: Spot::class, mappedBy: 'location', orphanRemoval: true)]
     private Collection $spot_id;
 
