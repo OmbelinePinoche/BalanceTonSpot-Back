8a04145545c6d73fcaeefdb9d5c5cb140267c607
DouyGatien <gatien.douy@oclock.school>
2024-02-16 15:51:09 +0100

    profilpicture works

--- migrations/Version20240216144730.php
+++ migrations/Version20240216144730.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240216144251 extends AbstractMigration
+final class Version20240216144730 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20240216144251 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user CHANGE profil_picture profilpicture VARCHAR(500) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user DROP rating');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user CHANGE profilpicture profil_picture VARCHAR(500) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD rating NUMERIC(2, 1) NOT NULL');
     }
 }
