32f1b119c0886ffaf022819e5d9d8335f5e1e488
OmbelinePinoche <ombeline.pinoche@oclock.school>
2024-02-19 16:12:07 +0100

    api user post update

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -54,7 +54,6 @@ class UserController extends AbstractController
         $user = new User();
         $user->setEmail($data['email']);
         $user->setPseudo($data['pseudo']);
-        $user->setProfilPicture($data['profilpicture']);
 
         // Hash password
         $hashedPassword = $passwordHasher->hashPassword($user, $data['password']);
