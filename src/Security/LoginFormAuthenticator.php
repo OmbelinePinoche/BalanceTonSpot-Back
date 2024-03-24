56fe4279f6a15f751bf826678dab945b46d62397
OmbelinePinoche <ombeline.pinoche@oclock.school>
2024-03-24 17:24:53 +0100

    backoffice authenticate function + css update

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -2,19 +2,23 @@
 
 namespace App\Security;
 
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\SecurityRequestAttributes;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 {
@@ -23,20 +27,32 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
     // Defines the route used for authentication
     public const LOGIN_ROUTE = 'app_login';
 
-    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    public function __construct(private UrlGeneratorInterface $urlGenerator, private CsrfTokenManagerInterface $csrfTokenManager, private UserRepository $userRepository, private ValidatorInterface $validator)
     {
     }
 
     // Method called to authenticate the user
     public function authenticate(Request $request): Passport
     {
-        // Gets the email from the request
         $email = $request->request->get('email', '');
+        $password = $request->request->get('password', '');
 
-        // Saves the email in the session to remember it on unsuccessful login attempts
-        $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $email);
+        // Validate the format of the email
+        $errors = $this->validator->validate($email, new Email());
 
-        // Creates a Passport object with a UserBadge, PasswordCredentials, and additional badges (CSRF, RememberMe)
+        if (count($errors) > 0) {
+            // The email is not valid
+            throw new CustomUserMessageAuthenticationException('Format d\'email invalide.');
+        }
+
+        // Search for the user by email
+        $user = $this->userRepository->findOneByEmail($email);
+
+        if (!$user || !password_verify($password, $user->getPassword())) {
+            throw new CustomUserMessageAuthenticationException('Email ou mot de passe incorrect.');
+        }
+
+        // Create a Passport object for authentication
         return new Passport(
             new UserBadge($email),
             new PasswordCredentials($request->request->get('password', '')),
